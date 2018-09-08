a3d6f725193549756cf5078013f881348f3ce75c
hannah-rae <hkerner@asu.edu>
2018-09-07 18:46:59 -0700

    Only show 3 plots for 3 datasets

--- plot/pca_all_datasets.py
+++ plot/pca_all_datasets.py
@@ -12,7 +12,7 @@ parser.add_argument('--normalize', action='store_true', help='normalize the data
 args = parser.parse_args()
 
 # Prepare the plot
-fig, axes = plt.subplots(nrows=4, ncols=2, sharey=True, sharex=True)
+fig, axes = plt.subplots(nrows=3, ncols=2, sharey=True, sharex=True)
 
 # Plot the principal components of full grid data
 data, _ = datasets.read_sim_data()
@@ -25,17 +25,24 @@ axes[0,0].step(datasets.time_bins_sim, pca.components_[1][:len(datasets.time_bin
 axes[0,0].step(datasets.time_bins_sim, pca.components_[2][:len(datasets.time_bins_sim)], where='post', linewidth=2, label='PC 3')
 axes[0,0].legend(loc='upper right')
 axes[0,0].set_xscale('log')
-axes[0,0].set_title('Thermal Principal Components (Full Simulation)')
+axes[0,0].set_title('Thermal Principal Components (Full Grid)')
 axes[0,0].set_xlabel('Time (us)')
-axes[0,0].set_ylabel('Normalized Counts')
+if args.normalize:
+    axes[0,0].set_ylabel('Normalized Counts')
+else:
+    axes[0,0].set_ylabel('Counts')
 axes[0,1].step(datasets.time_bins_sim, pca.components_[0][len(datasets.time_bins_sim):], where='post', linewidth=2, label='PC 1')
 axes[0,1].step(datasets.time_bins_sim, pca.components_[1][len(datasets.time_bins_sim):], where='post', linewidth=2, label='PC 2')
 axes[0,1].step(datasets.time_bins_sim, pca.components_[2][len(datasets.time_bins_sim):], where='post', linewidth=2, label='PC 3')
 axes[0,1].legend(loc='upper right')
 axes[0,1].set_xscale('log')
-axes[0,1].set_title('Epithermal Principal Components (Full Simulation)')
+axes[0,1].set_title('Epithermal Principal Components (Full Grid)')
 axes[0,1].set_xlabel('Time (us)')
-axes[0,1].set_ylabel('Normalized Counts')
+if args.normalize:
+    axes[0,1].set_ylabel('Normalized Counts')
+else:
+    axes[0,1].set_ylabel('Counts')
+
 
 # Plot the principal components of rover grid data
 data, _ = datasets.read_grid_data()
@@ -48,42 +55,55 @@ axes[1,0].step(datasets.time_bins_dan[:-1], pca.components_[1][:len(datasets.tim
 axes[1,0].step(datasets.time_bins_dan[:-1], pca.components_[2][:len(datasets.time_bins_dan)-1], where='post', linewidth=2, label='PC 3')
 axes[1,0].legend(loc='upper right')
 axes[1,0].set_xscale('log')
-axes[1,0].set_title('Thermal Principal Components (Rover Simulation)')
+axes[1,0].set_title('Thermal Principal Components (Rover Grid)')
 axes[1,0].set_xlabel('Time (us)')
-axes[1,0].set_ylabel('Normalized Counts')
+if args.normalize:
+    axes[1,0].set_ylabel('Normalized Counts')
+else:
+    axes[1,0].set_ylabel('Counts')
 axes[1,1].step(datasets.time_bins_dan[:-1], pca.components_[0][len(datasets.time_bins_dan)-1:], where='post', linewidth=2, label='PC 1')
 axes[1,1].step(datasets.time_bins_dan[:-1], pca.components_[1][len(datasets.time_bins_dan)-1:], where='post', linewidth=2, label='PC 2')
 axes[1,1].step(datasets.time_bins_dan[:-1], pca.components_[2][len(datasets.time_bins_dan)-1:], where='post', linewidth=2, label='PC 3')
 axes[1,1].legend(loc='upper right')
 axes[1,1].set_xscale('log')
-axes[1,1].set_title('Epithermal Principal Components (Rover Simulation)')
+axes[1,1].set_title('Epithermal Principal Components (Rover Grid)')
 axes[1,1].set_xlabel('Time (us)')
-axes[1,1].set_ylabel('Normalized Counts')
+if args.normalize:
+    axes[1,1].set_ylabel('Normalized Counts')
+else:
+    axes[1,1].set_ylabel('Counts')
 
 # Plot the principal components of combined full and rover grid data
-data_rover, _ = datasets.read_grid_data(limit_2000us=True)
-data_full, _ = datasets.read_sim_data(use_dan_bins=True)
-data = np.concatenate([data_full, data_rover])
-if args.normalize:
-    data = datasets.normalize_counts(data)
-pca = PCA(n_components=3)
-pca.fit(data)
-axes[2,0].step(datasets.time_bins_dan[:34], pca.components_[0][:34], where='post', linewidth=2, label='PC 1')
-axes[2,0].step(datasets.time_bins_dan[:34], pca.components_[1][:34], where='post', linewidth=2, label='PC 2')
-axes[2,0].step(datasets.time_bins_dan[:34], pca.components_[2][:34], where='post', linewidth=2, label='PC 3')
-axes[2,0].legend(loc='upper right')
-axes[2,0].set_xscale('log')
-axes[2,0].set_title('Thermal Principal Components (Rover Simulation)')
-axes[2,0].set_xlabel('Time (us)')
-axes[2,0].set_ylabel('Normalized Counts')
-axes[2,1].step(datasets.time_bins_dan[:34], pca.components_[0][34:], where='post', linewidth=2, label='PC 1')
-axes[2,1].step(datasets.time_bins_dan[:34], pca.components_[1][34:], where='post', linewidth=2, label='PC 2')
-axes[2,1].step(datasets.time_bins_dan[:34], pca.components_[2][34:], where='post', linewidth=2, label='PC 3')
-axes[2,1].legend(loc='upper right')
-axes[2,1].set_xscale('log')
-axes[2,1].set_title('Epithermal Principal Components (Rover Simulation)')
-axes[2,1].set_xlabel('Time (us)')
-axes[2,1].set_ylabel('Normalized Counts')
+# data_rover, _ = datasets.read_grid_data(limit_2000us=True)
+# data_full, _ = datasets.read_sim_data(use_dan_bins=True)
+# data = np.concatenate([data_full, data_rover])
+# if args.normalize:
+#     data = datasets.normalize_counts(data)
+# pca = PCA(n_components=3)
+# pca.fit(data)
+# axes[2,0].step(datasets.time_bins_dan[:34], pca.components_[0][:34], where='post', linewidth=2, label='PC 1')
+# axes[2,0].step(datasets.time_bins_dan[:34], pca.components_[1][:34], where='post', linewidth=2, label='PC 2')
+# axes[2,0].step(datasets.time_bins_dan[:34], pca.components_[2][:34], where='post', linewidth=2, label='PC 3')
+# axes[2,0].legend(loc='upper right')
+# axes[2,0].set_xscale('log')
+# axes[2,0].set_title('Thermal Principal Components (Rover + Full Grid)')
+# axes[2,0].set_xlabel('Time (us)')
+# if args.normalize:
+#     axes[2,0].set_ylabel('Normalized Counts')
+# else:
+#     axes[2,0].set_ylabel('Counts')
+# axes[2,1].step(datasets.time_bins_dan[:34], pca.components_[0][34:], where='post', linewidth=2, label='PC 1')
+# axes[2,1].step(datasets.time_bins_dan[:34], pca.components_[1][34:], where='post', linewidth=2, label='PC 2')
+# axes[2,1].step(datasets.time_bins_dan[:34], pca.components_[2][34:], where='post', linewidth=2, label='PC 3')
+# axes[2,1].legend(loc='upper right')
+# axes[2,1].set_xscale('log')
+# axes[2,1].set_title('Epithermal Principal Components (Rover + Full Grid)')
+# axes[2,1].set_xlabel('Time (us)')
+# if args.normalize:
+#     axes[2,1].set_ylabel('Normalized Counts')
+# else:
+#     axes[2,1].set_ylabel('Counts')
+
 
 # Plot principal components of DAN data
 data, _, _ = datasets.read_dan_data()
@@ -91,21 +111,26 @@ if args.normalize:
     data = datasets.normalize_counts(data)
 pca = PCA(n_components=3)
 pca.fit(data)
-axes[3,0].step(datasets.time_bins_dan[:-1], pca.components_[0][:len(datasets.time_bins_dan)-1], where='post', linewidth=2, label='PC 1')
-axes[3,0].step(datasets.time_bins_dan[:-1], pca.components_[1][:len(datasets.time_bins_dan)-1], where='post', linewidth=2, label='PC 2')
-axes[3,0].step(datasets.time_bins_dan[:-1], pca.components_[2][:len(datasets.time_bins_dan)-1], where='post', linewidth=2, label='PC 3')
-axes[3,0].legend(loc='upper right')
-axes[3,0].set_xscale('log')
-axes[3,0].set_title('Thermal Principal Components (DAN Actual)')
-axes[3,0].set_xlabel('Time (us)')
-axes[3,0].set_ylabel('Normalized Counts')
-axes[3,1].step(datasets.time_bins_dan[:-1], pca.components_[0][len(datasets.time_bins_dan)-1:], where='post', linewidth=2, label='PC 1')
-axes[3,1].step(datasets.time_bins_dan[:-1], pca.components_[1][len(datasets.time_bins_dan)-1:], where='post', linewidth=2, label='PC 2')
-axes[3,1].step(datasets.time_bins_dan[:-1], pca.components_[2][len(datasets.time_bins_dan)-1:], where='post', linewidth=2, label='PC 3')
-axes[3,1].legend(loc='upper right')
-axes[3,1].set_xscale('log')
-axes[3,1].set_title('Epithermal Principal Components (DAN Actual)')
-axes[3,1].set_xlabel('Time (us)')
-axes[3,1].set_ylabel('Normalized Counts')
-
+axes[2,0].step(datasets.time_bins_dan[:-1], pca.components_[0][:len(datasets.time_bins_dan)-1], where='post', linewidth=2, label='PC 1')
+axes[2,0].step(datasets.time_bins_dan[:-1], pca.components_[1][:len(datasets.time_bins_dan)-1], where='post', linewidth=2, label='PC 2')
+axes[2,0].step(datasets.time_bins_dan[:-1], pca.components_[2][:len(datasets.time_bins_dan)-1], where='post', linewidth=2, label='PC 3')
+axes[2,0].legend(loc='upper right')
+axes[2,0].set_xscale('log')
+axes[2,0].set_title('Thermal Principal Components (DAN Actual)')
+axes[2,0].set_xlabel('Time (us)')
+if args.normalize:
+    axes[2,0].set_ylabel('Normalized Counts')
+else:
+    axes[2,0].set_ylabel('Counts')
+axes[2,1].step(datasets.time_bins_dan[:-1], pca.components_[0][len(datasets.time_bins_dan)-1:], where='post', linewidth=2, label='PC 1')
+axes[2,1].step(datasets.time_bins_dan[:-1], pca.components_[1][len(datasets.time_bins_dan)-1:], where='post', linewidth=2, label='PC 2')
+axes[2,1].step(datasets.time_bins_dan[:-1], pca.components_[2][len(datasets.time_bins_dan)-1:], where='post', linewidth=2, label='PC 3')
+axes[2,1].legend(loc='upper right')
+axes[2,1].set_xscale('log')
+axes[2,1].set_title('Epithermal Principal Components (DAN Actual)')
+axes[2,1].set_xlabel('Time (us)')
+if args.normalize:
+    axes[2,1].set_ylabel('Normalized Counts')
+else:
+    axes[2,1].set_ylabel('Counts')
 plt.show()
