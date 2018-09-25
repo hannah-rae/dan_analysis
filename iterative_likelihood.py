ec94e28fc0023b456633845635f33c3946d06873
hannah-rae <hkerner@asu.edu>
2018-09-25 15:30:22 -0700

    Option to restrict bins

--- iterative_likelihood.py
+++ iterative_likelihood.py
@@ -8,10 +8,14 @@ import datasets
 # Parse command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_components', type=int, default=3, help='number of principal components to use for PCA')
+parser.add_argument('--use_restricted_bins', action='store_true', help='only use bins 18-34 and 13-17 for thermal and epithermal')
 args = parser.parse_args()
 
 X, Y, Y_err, names = datasets.read_dan_data()
 n_meas = X.shape[0]
+
+if args.use_restricted_bins:
+    X = np.take(X, range(17, 34)+range(64+12, 64+17), axis=1)
 
 scores = []
 for i in range(n_meas):
@@ -36,6 +40,7 @@ ax2.plot(range(1,len(scores)+1), Y[:,1], label='Wt % Cl (IKI)')
 ax2.set_title("H and Cl Abundances")
 ax2.set_xlabel("Measurement")
 ax2.set_ylabel("Wt %")
+ax2.legend(loc='upper left')
 
 # Allow user to click on points and print which measurement the point belongs to
 def onpick(event):
--- plot/plot_one_curve.py
+++ plot/plot_one_curve.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt 
 import numpy as np
+from sklearn.decomposition import PCA
 import argparse
 
 import datasets
@@ -9,23 +10,52 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--meas_id', help='identifier for mesaurement to plot, e.g. 428122068EAC03450100288')
 parser.add_argument('--plot_mean', action='store_true', help='plot the mean epithermal and thermal die-away curve')
 parser.add_argument('--plot_until', action='store_true', help='plot the mean epithermal and thermal die-away curve to date')
+parser.add_argument('--plot_recon', action='store_true', help='plot the curve reconstructed from PCs of curves to date')
+parser.add_argument('--use_restricted_bins', action='store_true', help='only use bins 18-34 and 13-17 for thermal and epithermal')
 args = parser.parse_args()
 
 X, Y, Y_err, names = datasets.read_dan_data()
+if args.use_restricted_bins:
+    X = np.take(X, range(17, 34)+range(64+12, 64+17), axis=1)
 X_avg = np.mean(X, axis=0)
 idx = np.where(np.array(names) == args.meas_id)
 X_avg_todate = np.mean(X[:idx[0][0]], axis=0)
 
 fig, (ax1, ax2) = plt.subplots(2)
 
-ax1.step(datasets.time_bins_dan[:-1], X[idx][0][:64], where='post', linewidth=2, label='%s' % args.meas_id)
-ax2.step(datasets.time_bins_dan[:-1], X[idx][0][64:], where='post', linewidth=2, label='%s' % args.meas_id)
-if args.plot_mean:
-    ax1.step(datasets.time_bins_dan[:-1], X_avg[:64], where='post', linewidth=2, label='Mean curve')
-    ax2.step(datasets.time_bins_dan[:-1], X_avg[64:], where='post', linewidth=2, label='Mean curve')
-if args.plot_until:
-    ax1.step(datasets.time_bins_dan[:-1], X_avg_todate[:64], where='post', linewidth=2, label='Mean Curve To Date')
-    ax2.step(datasets.time_bins_dan[:-1], X_avg_todate[64:], where='post', linewidth=2, label='Mean Curve To Date')
+if args.use_restricted_bins:
+    n_th_bins = len(range(17, 34))
+    ax1.step(datasets.time_bins_dan[17:34], X[idx][0][:n_th_bins], where='post', linewidth=2, label='%s' % args.meas_id)
+    ax2.step(datasets.time_bins_dan[12:17], X[idx][0][n_th_bins:], where='post', linewidth=2, label='%s' % args.meas_id)
+    if args.plot_mean:
+        ax1.step(datasets.time_bins_dan[17:34], X_avg[:n_th_bins], where='post', linewidth=2, label='Mean curve')
+        ax2.step(datasets.time_bins_dan[12:17], X_avg[n_th_bins:], where='post', linewidth=2, label='Mean curve')
+    if args.plot_until:
+        ax1.step(datasets.time_bins_dan[17:34], X_avg_todate[:n_th_bins], where='post', linewidth=2, label='Mean Curve To Date')
+        ax2.step(datasets.time_bins_dan[12:17], X_avg_todate[n_th_bins:], where='post', linewidth=2, label='Mean Curve To Date')
+    if args.plot_recon:
+        pca = PCA(n_components=3)
+        pca.fit(X[:idx[0][0]])
+        x_recon = pca.inverse_transform([pca.transform([X[idx][0]])])[0][0]
+        print x_recon.shape
+        ax1.step(datasets.time_bins_dan[17:34], x_recon[:n_th_bins], where='post', linewidth=2, label='Reconstructed')
+        ax2.step(datasets.time_bins_dan[12:17], x_recon[n_th_bins:], where='post', linewidth=2, label='Reconstructed')
+else:
+    ax1.step(datasets.time_bins_dan[:-1], X[idx][0][:64], where='post', linewidth=2, label='%s' % args.meas_id)
+    ax2.step(datasets.time_bins_dan[:-1], X[idx][0][64:], where='post', linewidth=2, label='%s' % args.meas_id)
+    if args.plot_mean:
+        ax1.step(datasets.time_bins_dan[:-1], X_avg[:64], where='post', linewidth=2, label='Mean curve')
+        ax2.step(datasets.time_bins_dan[:-1], X_avg[64:], where='post', linewidth=2, label='Mean curve')
+    if args.plot_until:
+        ax1.step(datasets.time_bins_dan[:-1], X_avg_todate[:64], where='post', linewidth=2, label='Mean Curve To Date')
+        ax2.step(datasets.time_bins_dan[:-1], X_avg_todate[64:], where='post', linewidth=2, label='Mean Curve To Date')
+    if args.plot_recon:
+        pca = PCA(n_components=3)
+        pca.fit(X[:idx[0][0]])
+        x_recon = pca.inverse_transform([pca.transform([X[idx][0]])])[0][0]
+        print x_recon.shape
+        ax1.step(datasets.time_bins_dan[:-1], x_recon[:64], where='post', linewidth=2, label='Reconstructed')
+        ax2.step(datasets.time_bins_dan[:-1], x_recon[64:], where='post', linewidth=2, label='Reconstructed')
 
 ax1.set_xscale('log')
 ax1.legend(loc='lower left')
