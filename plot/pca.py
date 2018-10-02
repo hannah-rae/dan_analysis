0e2e1ba9e99e4ac8d2c4350cfb841035d2d4c2d7
hannah-rae <hkerner@asu.edu>
2018-10-02 16:16:12 -0700

    Plot the curves corresponding to points in PC space

--- plot/pca.py
+++ plot/pca.py
@@ -15,15 +15,23 @@ args = parser.parse_args()
 
 if args.dataset == 'rover':
     X, Y = datasets.read_grid_data()
+    time_bins = datasets.time_bins_dan
+    n_bins = 64
 elif args.dataset == 'full':
     X, Y = datasets.read_sim_data()
+    time_bins = datasets.time_bins_sim
+    n_bins = 131
 elif args.dataset == 'dan':
     X, Y, err, names = datasets.read_dan_data()
+    time_bins = datasets.time_bins_dan
+    n_bins = 64
 elif args.dataset == 'both_models':
     X_rover, Y_rover = datasets.read_grid_data(limit_2000us=True)
     X_full, Y_full = datasets.read_sim_data(use_dan_bins=True)
     X = np.concatenate([X_full, X_rover])
     Y = np.concatenate([Y_rover, Y_full])
+    time_bins = datasets.time_bins_dan
+    n_bins = 64
 
 if args.normalize:
     X = datasets.normalize_counts(X)
@@ -34,7 +42,9 @@ X_t = pca.fit_transform(X)
 # Plot clusters in PC space
 if args.n_components >= 3:
     fig = plt.figure()
-    ax1 = fig.add_subplot(111, projection='3d')
+    ax1 = fig.add_subplot(1, 3, 1, projection='3d')
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax3 = fig.add_subplot(1, 3, 3)
     ax1.set_xlabel('PC 1')
     ax1.set_ylabel('PC 2')
     ax1.set_zlabel('PC 3')
@@ -45,11 +55,37 @@ elif args.n_components == 2:
     ax1.set_ylabel('PC 2')
     ax1.scatter(X_t[:,0], X_t[:,1], picker=True)
 
+# Plot original data
+def plot_original(i):
+    h, cl = Y[i]
+    ax2.step(time_bins[:-1], X[i, :n_bins], where='post', linewidth=2,label=' %0.2f H  %0.2f Cl' % (h, cl))
+    ax3.step(time_bins[:-1], X[i, n_bins:], where='post', linewidth=2,label=' %0.2f H  %0.2f Cl' % (h, cl))
+    ax2.set_xscale('log')
+    ax2.legend(loc='lower left')
+    ax2.set_xlabel('Time (us)')
+    ax2.set_ylabel('Counts')
+    ax2.set_title("Thermal Neutron Die-Away Curve")
+    ax2.set_ylim(np.min(X), np.max(X))
+    ax3.set_xscale('log')
+    ax3.legend(loc='lower left')
+    ax3.set_xlabel('Time (us)')
+    ax3.set_ylabel('Counts')
+    ax3.set_title("Epithermal Neutron Die-Away Curve")
+    ax3.set_ylim(np.min(X), np.max(X))
+
+def plot_curves(indexes):
+    ax2.cla()
+    ax3.cla()
+    for i in indexes: # might be more than one point if ambiguous click
+        plot_original(i)
+    plt.draw()
+
 # Allow user to click on points and print which measurement the point belongs to
 def onpick(event):
     ind = event.ind
     # print Y_test[ind[0]]
     print Y[ind]
+    plot_curves(list(ind))
 
 fig.canvas.mpl_connect('pick_event', onpick)
 
