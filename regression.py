40927340694fe3f74232b477da32b4c1a68dd6db
hannah-rae <hkerner@asu.edu>
2018-11-12 13:32:49 -0700

    Compute H and ACS only cross-validated metrics

--- regression.py
+++ regression.py
@@ -4,6 +4,8 @@ so the results represent the behavior of neutron die-away curves in the absence 
 detectors and electronics. Effectively, these outputs represent the theoretical behavior 
 of thermal and epithermal neutrons after a 14.1 MeV neutron pulse.
 '''
+import matplotlib
+matplotlib.use('TKAgg')
 
 import numpy as np
 import scipy
@@ -17,11 +19,15 @@ from mpl_toolkits.mplot3d import Axes3D
 import sklearn
 from sklearn.decomposition import PCA, FastICA
 from sklearn.cluster import KMeans
-from sklearn.metrics import r2_score
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.model_selection import cross_val_score, ShuffleSplit
 from sklearn import preprocessing
 
 import datasets
+
+plt.rc('font', family='serif')
+plt.rc('xtick', labelsize='x-small')
+plt.rc('ytick', labelsize='x-small')
 
 # Parse command line arguments
 parser = argparse.ArgumentParser()
@@ -29,7 +35,8 @@ parser.add_argument('--n_components', type=int, default=3, help='number of princ
 parser.add_argument('--testing_percentage', type=int, default=20, help='percent of training data to use for testing')
 parser.add_argument('--epsilon', type=float, default=0, help='wiggle room for accuracy')
 parser.add_argument('--test_dan', action='store_true', help='use DAN data for testing')
-parser.add_argument('--test_sim', action='store_true', help='use percentage of training data for testing')
+parser.add_argument('--test_sebina', action='store_true', help='use percentage of Sebina grid data for testing')
+parser.add_argument('--test_sim', action='store_true', help='use percentage of Hardgrove sim data for testing')
 parser.add_argument('--linear', action='store_true', help='perform linear regression to predict H and Cl values')
 parser.add_argument('--dnn', action='store_true', help='perform regression using deep neural network to predict H and Cl values')
 parser.add_argument('--lasso', action='store_true', help='perform lasso regression to predict H and Cl values')
@@ -39,7 +46,7 @@ parser.add_argument('--use_restricted_bins', action='store_true', help='only use
 parser.add_argument('--plot_error_bars', action='store_true', help='plot error bars for H and Cl values (DAN data)')
 parser.add_argument('--plot_intermediate_steps', action='store_true', help='visualize steps through regression process')
 parser.add_argument('--pca', action='store_true', help='whether to use PCA to reduce dimensions or not')
-parser.add_argument('--model_grid', help='Which model dataset to use: options are rover, full, or both')
+parser.add_argument('--model_grid', help='Which model dataset to use: options are sabina, hardgrove2011, both, or acs')
 args = parser.parse_args()
 
 def get_accuracy(y_pred, y_true, y_pred_err, epsilon):
@@ -62,14 +69,25 @@ if args.test_sim:
     Y_test = Y[:n_test]
     X_train = X[n_test:]
     Y_train = Y[n_test:]
+elif args.test_sebina:
+    X, Y = datasets.read_acs_grid_data()
+    n_test = int(X.shape[0] * args.testing_percentage/100.0)
+    X_test = X[:n_test]
+    Y_test = Y[:n_test]
+    X_train = X[n_test:]
+    Y_train = Y[n_test:]
 elif args.test_dan:
-    if args.model_grid == 'full':
+    if args.model_grid == 'hardgrove2011':
         X, Y = datasets.read_sim_data(use_dan_bins=True)
         X_test, Y_test, Y_test_error, test_names = datasets.read_dan_data(limit_2000us=True)
         n_bins = 34
-    elif args.model_grid == 'rover':
+    elif args.model_grid == 'sabina':
         X, Y = datasets.read_grid_data()
-        X_test, Y_test, Y_test_error, test_names = datasets.read_dan_data(limit_2000us=False)
+        X_test, Y_test, Y_test_error, test_names = datasets.read_dan_data(limit_2000us=False, label_source='iki')
+        n_bins = len(datasets.time_bins_dan)-1
+    elif args.model_grid == 'acs':
+        X, Y = datasets.read_acs_grid_data()
+        X_test, Y_test, Y_chi2, test_names = datasets.read_dan_data(limit_2000us=False, label_source='asu')
         n_bins = len(datasets.time_bins_dan)-1
     elif args.model_grid == 'both':
         X_full, Y_full = datasets.read_sim_data(use_dan_bins=True)
@@ -101,16 +119,17 @@ if args.use_restricted_bins:
 print X_train.shape
 print X_test.shape
 
-print 'train min %f' % np.min(X_train)
-print 'train max %f' % np.max(X_train)
-print 'test min %f' % np.min(X_test)
-print 'test max %f' % np.max(X_test)
+print 'train mean %f' % np.mean(X_train)
+print 'train std %f' % np.std(X_train)
+print 'test mean %f' % np.mean(X_test)
+print 'test std %f' % np.std(X_test)
 
 if args.pca:
     # Fit PCA model and project data into PC space
     pca = PCA(n_components=args.n_components)
     pca.fit(X_train)
     X_train = pca.transform(X_train)
+    pca_scores = pca.score_samples(X_test)
     X_test = pca.transform(X_test)
 
 if args.plot_intermediate_steps:
@@ -120,14 +139,14 @@ if args.plot_intermediate_steps:
     ax0.set_xlabel('PC 1')
     ax0.set_ylabel('PC 2')
     ax0.set_zlabel('PC 3')
-    ax0.scatter(X_train[:,0], X_train[:,1], X_train[:,2], label='Train')
-    ax0.scatter(X_test[:,0], X_test[:,1], X_test[:,2], label='Test')
+    ax0.scatter(X_train[:,0], X_train[:,1], X_train[:,2], color='k', label='Model (Train)')
+    ax0.scatter(X_test[:,0], X_test[:,1], X_test[:,2], color='red', label='DAN (Test)')
     ax0.legend(loc='upper right')
 
 if args.plot_intermediate_steps:
-    if args.test_dan and args.model_grid == 'rover':
+    if args.test_dan and (args.model_grid == 'sabina' or args.model_grid == 'acs'):
         time_bins = datasets.time_bins_dan
-    elif args.test_dan and (args.model_grid == 'full' or args.model_grid == 'both'):
+    elif args.test_dan and (args.model_grid == 'hardgrove2011' or args.model_grid == 'both'):
         time_bins = datasets.time_bins_dan[:34]
     elif args.test_sim:
         time_bins = datasets.time_bins_sim
@@ -161,8 +180,26 @@ if args.plot_intermediate_steps:
 if args.linear:
     from sklearn.linear_model import LinearRegression
     lr = LinearRegression()
-    print np.mean(cross_val_score(lr, X_train, np.log(Y_train+1), cv=5, scoring=sklearn.metrics.make_scorer(r2_score)))
-    
+    print np.mean(cross_val_score(lr, X, Y, cv=5, scoring=sklearn.metrics.make_scorer(r2_score)))
+    print np.mean(cross_val_score(lr, X, Y, cv=5, scoring=sklearn.metrics.make_scorer(mean_absolute_error)))
+
+    # cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
+    # mean_h_r2 = 0
+    # mean_acs_r2 = 0
+    # mean_h_mae = 0
+    # mean_acs_mae = 0
+    # for train_index, test_index in cv.split(X):
+    #     lr.fit(X=X[train_index], y=Y[train_index])
+    #     Y_pred = lr.predict(X=X[test_index])
+    #     mean_h_r2 += r2_score(Y[test_index][:,0], Y_pred[:,0])
+    #     mean_acs_r2 += r2_score(Y[test_index][:,1], Y_pred[:,1])
+    #     mean_h_mae += mean_absolute_error(Y[test_index][:,0], Y_pred[:,0])
+    #     mean_acs_mae += mean_absolute_error(Y[test_index][:,1], Y_pred[:,1])
+    # print "Overall H only R2 = %f" % (mean_h_r2 / 5.)
+    # print "Overall ACS only R2 = %f" % (mean_acs_r2 / 5.)
+    # print "Overall H only MAE = %f" % (mean_h_mae / 5.)
+    # print "Overall ACS only MAE = %f" % (mean_acs_mae / 5.)
+
     lr.fit(X=X_train, y=np.log(Y_train+1))
     #lr.fit(X=X_train, y=Y_train)
 
@@ -230,31 +267,70 @@ if args.linear:
         # #     if mask[i] == 1:
         # #         print test_names[i]
         # # np.savetxt('mask.txt', mask)
-    else:
-        print "H accuracy = %f" % get_accuracy(Y_pred[:,0], Y_test[:,0], np.zeros(Y_pred[:,0].shape))[0]
-        print "Cl accuracy = %f" % get_accuracy(Y_pred[:,1], Y_test[:,1], np.zeros(Y_pred[:,1].shape))[0]
-        print "Incorrect H:"
-        mask = get_accuracy(Y_pred[:,0], Y_test[:,0])[1]
-        for i in range(len(mask)):
-            if mask[i] == 1:
-                print test_names[i]
+    # else:
+    #     print "H accuracy = %f" % get_accuracy(Y_pred[:,0], Y_test[:,0], np.zeros(Y_pred[:,0].shape), epsilon=args.epsilon)[0]
+    #     print "Cl accuracy = %f" % get_accuracy(Y_pred[:,1], Y_test[:,1], np.zeros(Y_pred[:,1].shape), epsilon=args.epsilon)[0]
+    #     epsilon = np.arange(0, 3.1, 0.1) 
+    #     figb, (acc_vis, chiscatter) = plt.subplots(2)
+    #     acc_vis.plot(epsilon, [get_accuracy(Y_pred[:,0], Y_test[:,0], np.zeros(Y_pred[:,0].shape), epsilon=e)[0] for e in epsilon], label='H')
+    #     acc_vis.plot(epsilon, [get_accuracy(Y_pred[:,1], Y_test[:,1], np.zeros(Y_pred[:,1].shape), epsilon=e)[0] for e in epsilon], label='ACS')
+    #     acc_vis.set_xlabel('Epsilon (wiggle room on accuracy)')
+    #     acc_vis.set_ylabel('Accuracy as a Function of Epsilon')
+    #     acc_vis.legend(loc='upper left')
+
+    #     sols = [int(name.split('EAC')[-1][:4]) for name in test_names]
+    #     sc = chiscatter.scatter(Y_chi2, np.abs(np.subtract(Y_pred[:,0], Y_test[:,0])), label='H', picker=True, c=sols, alpha=0.7)
+    #     #chiscatter.scatter(np.abs(np.subtract(Y_pred[:,1], Y_test[:,1])), Y_chi2, label='ACS', picker=True)
+    #     chiscatter.set_ylabel('Regression Error')
+    #     chiscatter.set_xlabel('Number of Good Fits')
+    #     chiscatter.set_title('Regression Error vs. # Good Fits, Colored by Sol')
+    #     #chiscatter.legend(loc='upper right')
+    #     figb.colorbar(sc, ax=chiscatter)
+        # score_scatter.scatter(np.abs(np.subtract(Y_pred[:,0], Y_test[:,0])), pca_scores, label='H', picker=True)
+        # score_scatter.scatter(np.abs(np.subtract(Y_pred[:,1], Y_test[:,1])), pca_scores, label='ACS', picker=True)
+        # score_scatter.set_xlabel('Regression Error')
+        # score_scatter.set_ylabel('Likelihood of Measumrent under PCA Model')
+        # score_scatter.legend(loc='upper right')
+        # sols = [int(name.split('EAC')[-1][:4]) for name in test_names]
+        # score_scatter.scatter(sols, np.abs(np.subtract(Y_pred[:,0], Y_test[:,0])), label='H', picker=True)
+        # score_scatter.scatter(sols, np.abs(np.subtract(Y_pred[:,1], Y_test[:,1])), label='ACS', picker=True)
+        # score_scatter.set_ylabel('Regression Error')
+        # score_scatter.set_xlabel('Sol')
+        # score_scatter.legend(loc='upper right')
+
+
+        # print "Incorrect H:"
+        # mask = get_accuracy(Y_pred[:,0], Y_test[:,0])[1]
+        # for i in range(len(mask)):
+        #     if mask[i] == 1:
+        #         print test_names[i]
 
 
     # Plot a scatter plot to show correlations
     fig, (ax3, ax4) = plt.subplots(nrows=1, ncols=2)
-    ax3.scatter(Y_test[:, 0], Y_pred[:, 0], picker=True)
+    ax3.scatter(Y_test[:, 0], Y_pred[:, 0], picker=True, color='k')
     ax3.set_ylabel('Predicted H value (wt %)')
     ax3.set_xlabel('Actual H value (wt %)')
-    ax4.scatter(Y_test[:, 1], Y_pred[:, 1], picker=True)
-    ax4.set_ylabel('Predicted Cl value (wt %)')
-    ax4.set_xlabel('Actual Cl value (wt %)')
+    ax3.set_title('Correlation of Actual and \nPredicted H')
+    print "H R^2: %f" % r2_score(Y_test[:,0], Y_pred[:,0])
+    print "H MAE: %f" % mean_absolute_error(Y_test[:,0], Y_pred[:,0])
+    ax4.scatter(Y_test[:, 1], Y_pred[:, 1], picker=True, color='k')
+    ax4.set_ylabel('Predicted ACS value (barns)')
+    ax4.set_xlabel('Actual ACS value (barns)')
+    ax4.set_title('Correlation of Actual and \nPredicted ACS')
+    print "ACS R^2: %f" % r2_score(Y_test[:,1], Y_pred[:,1])
+    print "ACS MAE: %f" % mean_absolute_error(Y_test[:,1], Y_pred[:,1])
     # Allow user to click on points and print which measurement the point belongs to
     def onpick(event):
         ind = event.ind
         # print Y_test[ind[0]]
         print Y_test[ind]
+        print ind
+        if args.model_grid == 'acs':
+            print [test_names[i] for i in ind]
 
     fig.canvas.mpl_connect('pick_event', onpick)
+    #figb.canvas.mpl_connect('pick_event', onpick)
 
     if args.plot_intermediate_steps:
         fig, rvis_test = plt.subplots(nrows=args.n_components, ncols=2)
