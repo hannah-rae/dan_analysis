5d5851c0eb0ab7b2dc851ce6d1701f43227fae55
hannah-rae <hkerner@asu.edu>
2019-05-06 11:07:45 -0700

    Clean up and add comments

--- plot/fig1.py
+++ plot/fig1.py
@@ -3,13 +3,12 @@ import matplotlib.pyplot as plt
 
 import datasets
 
-# plt.rc('font', family='serif')
-# plt.rc('xtick', labelsize='x-small')
-# plt.rc('ytick', labelsize='x-small')
 plt.rc('font', family='Arial', size=10)
 
+# Load all the DAN data
 X, Y, X_error, names = datasets.read_dan_data()
 
+# Specify which DAN measurements we want to plot
 h1 = 'DNB_456787389EAC06680361170_______M1' # 2.6 H, 1.123 ACS
 h2 = 'DNB_455739444EAC06560341120_______M1' # 3.3 H, 1.123 ACS
 
@@ -22,45 +21,35 @@ h2_idx = np.where(names == h2)
 acs1_idx = np.where(names == acs1)
 acs2_idx = np.where(names == acs2)
 
-fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
-time_bins_m = [np.mean([datasets.time_bins_dan[t], datasets.time_bins_dan[t+1]]) for t in range(len(datasets.time_bins_dan)-1)]
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(15,5))
+
+# The last time bin is the end of the last bin
+time_bins = datasets.time_bins_dan[:-1]
+
+# Avoid error with x-axis log scale
+time_bins[0] = 1e-20
+
 # Thermal die-away curves
-ax1.step(datasets.time_bins_dan[:-1], X[h1_idx][0][:64], where='post', linewidth=2, label='H: 2.6 wt %, $\Sigma_{abs}$: 1.123 b', color='k')
-ax1.step(datasets.time_bins_dan[:-1], X[h2_idx][0][:64], where='post', linewidth=2, label='H: 3.3 wt %, $\Sigma_{abs}$: 1.123 b', color='blue')
-ax1.step(datasets.time_bins_dan[:-1], X[acs2_idx][0][:64], where='post', linewidth=2, label='H: 2.6 wt %, $\Sigma_{abs}$: 0.93 b', color='purple')
+ax1.step(time_bins, X[h1_idx][0][:64], where='post', linewidth=2, label='WEH: 2.6 wt. %, $\Sigma_{abs}$: 1.123 b', color='k')
+ax1.step(time_bins, X[h2_idx][0][:64], where='post', linewidth=2, label='WEH: 3.3 wt. %, $\Sigma_{abs}$: 1.123 b', color='blue')
+ax1.step(time_bins, X[acs2_idx][0][:64], where='post', linewidth=2, label='WEH: 2.6 wt. %, $\Sigma_{abs}$: 0.93 b', color='purple')
 
 # Epithermal die-away curves
-ax2.step(datasets.time_bins_dan[:-1], X[h1_idx][0][64:], where='post', linewidth=2, label='H: 2.6 wt %, $\Sigma_{abs}$: 1.123 b', color='k')
-ax2.step(datasets.time_bins_dan[:-1], X[h2_idx][0][64:], where='post', linewidth=2, label='H: 3.3 wt %, $\Sigma_{abs}$: 1.123 b', color='blue')
-ax2.step(datasets.time_bins_dan[:-1], X[acs2_idx][0][64:], where='post', linewidth=2, label='H: 2.6 wt %, $\Sigma_{abs}$: 0.93 b', color='purple')
+ax2.step(time_bins, X[h1_idx][0][64:], where='post', linewidth=2, label='WEH: 2.6 wt. %, $\Sigma_{abs}$: 1.123 b', color='k')
+ax2.step(time_bins, X[h2_idx][0][64:], where='post', linewidth=2, label='WEH: 3.3 wt. %, $\Sigma_{abs}$: 1.123 b', color='blue')
+ax2.step(time_bins, X[acs2_idx][0][64:], where='post', linewidth=2, label='WEH: 2.6 wt. %, $\Sigma_{abs}$: 0.93 b', color='purple')
 
-
-# ax2.step(datasets.time_bins_dan[:-1], X[idx][0][64:], where='post', linewidth=2, label='%s' % args.meas_id)
-# if args.plot_mean:
-#     ax1.step(datasets.time_bins_dan[:-1], X_avg[:64], where='post', linewidth=2, label='Mean curve')
-#     ax2.step(datasets.time_bins_dan[:-1], X_avg[64:], where='post', linewidth=2, label='Mean curve')
-# if args.plot_until:
-#     ax1.step(datasets.time_bins_dan[:-1], X_avg_todate[:64], where='post', linewidth=2, label='Mean Curve To Date')
-#     ax2.step(datasets.time_bins_dan[:-1], X_avg_todate[64:], where='post', linewidth=2, label='Mean Curve To Date')
-# if args.plot_recon:
-#     pca = PCA(n_components=3)
-#     pca.fit(X[:idx[0][0]])
-#     x_recon = pca.inverse_transform([pca.transform([X[idx][0]])])[0][0]
-#     print x_recon.shape
-#     ax1.step(datasets.time_bins_dan[:-1], x_recon[:64], where='post', linewidth=2, label='Reconstructed')
-#     ax2.step(datasets.time_bins_dan[:-1], x_recon[64:], where='post', linewidth=2, label='Reconstructed')
-
-
-# # Set figure parameters
+# Set figure parameters
 ax1.set_xscale('log')
 ax1.legend(loc='best')
-ax1.set_xlabel('Time (us)')
-ax1.set_ylabel('Thermal Neutron Counts')
-ax1.set_title("Effect of H and $\Sigma_{abs}$ on Thermal Die-Away Curves")
+ax1.set_xlabel('Time ($\mu$s)')
+ax1.set_ylabel('Thermal Neutrons (neutrons)')
+ax1.set_title("Effect of WEH and $\Sigma_{abs}$ on Thermal Die-Away Curves")
+
 ax2.set_xscale('log')
 ax2.legend(loc='best')
-ax2.set_xlabel('Time (us)')
-ax2.set_ylabel('Epithermal Neutron Counts')
-ax2.set_title("Effect of H and $\Sigma_{abs}$ on Epithermal Die-Away Curves")
-
+ax2.set_xlabel('Time ($\mu$s)')
+ax2.set_ylabel('Epithermal Neutrons (neutrons)')
+ax2.set_title("Effect of WEH and $\Sigma_{abs}$ on Epithermal Die-Away Curves")
+plt.savefig('/Users/hannahrae/Documents/Grad School/DAN Manuscript/figures/example_curves_horiz.pdf', transparent=True)
 plt.show()
