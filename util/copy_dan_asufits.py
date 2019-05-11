baffaf7c697ec8f6534924ffd829ef61af4e50b9
hannah-rae <hkerner@asu.edu>
2019-05-11 11:33:08 -0700

    Copy fit data for DAN measurements against high WEH grid

--- util/copy_dan_asufits.py
+++ util/copy_dan_asufits.py
@@ -2,7 +2,7 @@ import os.path
 from glob import glob
 from subprocess import call
 
-out_dir = '/Users/danops/Desktop/dan_asufits_hrk'
+out_dir = '/Users/danops/Desktop/dan_asufits_hiweh'
 
 for soldir in glob('/Users/danops/Desktop/DANOPS/FEI/DAN/sol*'):
     # Make a new directory for this sol
@@ -15,8 +15,8 @@ for soldir in glob('/Users/danops/Desktop/DANOPS/FEI/DAN/sol*'):
         # Copy the background-subtracted data
         call(['cp', os.path.join(mdir, 'bg_dat.npy'), os.path.join(out_dir, sol, mname)])
         # Copy the best-fit model information
-        call(['cp', os.path.join(mdir, 'asu1/M1R_homogeneous_APXS_SB_FT_0.1-6.0H_0.48-14.0Fe_0.1-3.0Cl_1.8rho_MASTER/results/gridInfo_statistics.csv'), os.path.join(out_dir, sol, mname)])
-        call(['cp', os.path.join(mdir, 'asu1/M1R_homogeneous_APXS_SB_FT_0.1-6.0H_0.48-14.0Fe_0.1-3.0Cl_1.8rho_MASTER/results/gridInfo.csv'), os.path.join(out_dir, sol, mname)])
-        call(['cp', os.path.join(mdir, 'asu1/M1R_homogeneous_APXS_SB_FT_0.1-6.0H_0.48-14.0Fe_0.1-3.0Cl_1.8rho_MASTER/results/gridInfo_goodFitList.csv'), os.path.join(out_dir, sol, mname)])
+        call(['cp', os.path.join(mdir, 'asu1/M1R_homogeneous_APXS_SB_FT_0.1-25.0H_0.48-14.22Fe_0.1-3.0Cl_1.8rho_MASTER_HiWEH/results/gridInfo_statistics.csv'), os.path.join(out_dir, sol, mname)])
+        call(['cp', os.path.join(mdir, 'asu1/M1R_homogeneous_APXS_SB_FT_0.1-25.0H_0.48-14.22Fe_0.1-3.0Cl_1.8rho_MASTER_HiWEH/results/gridInfo.csv'), os.path.join(out_dir, sol, mname)])
+        call(['cp', os.path.join(mdir, 'asu1/M1R_homogeneous_APXS_SB_FT_0.1-25.0H_0.48-14.22Fe_0.1-3.0Cl_1.8rho_MASTER_HiWEH/results/gridInfo_goodFitList.csv'), os.path.join(out_dir, sol, mname)])
         call('cp %s %s' % (os.path.join(mdir, '*.JPG'), os.path.join(out_dir, sol, mname)), shell=True)
         call('cp %s %s' % (os.path.join(mdir, '*.png'), os.path.join(out_dir, sol, mname)), shell=True)
