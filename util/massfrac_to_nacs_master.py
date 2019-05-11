46926b3c6dd8004b72c2c8b86f0cd498866c0e55
hannah-rae <hkerner@asu.edu>
2019-05-11 11:34:23 -0700

    Compute bulk absorption cross section for master grid

--- util/massfrac_to_nacs_master.py
+++ util/massfrac_to_nacs_master.py
@@ -1,18 +1,31 @@
 import os.path
 import pandas as pd
+import argparse
 from glob import glob
 from subprocess import call
+from scipy.constants import N_A
 
 material = 'm7010'
 nacs_by_iso = pd.read_csv('/Users/hannahrae/data/dan/BNACS_by_isotope.csv')
 
-#output_dir = '/Users/hannahrae/data/dan/M1R_homogeneous_APXS_SB_FT_0.1-6.0H_0.48-14.0Fe_1.8rho_complete/mcnp_outputs_nacs'
-#mcnp_dir = '/Users/hannahrae/data/dan/M1R_homogeneous_APXS_SB_FT_0.1-6.0H_0.48-14.0Fe_1.8rho_complete/mcnp_outputs'
-# cl_grid = False # cl_grid is False with Fe-variation grid
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--sink_dir', help='Path to directory to save renamed MCNP output files')
+parser.add_argument('--source_dir', help='Path to MCNP output files to rename')
+parser.add_argument('--cl_grid', action='store_true', help='Set if this is a Cl-varying grid')
+parser.add_argument('--fe_grid', action='store_true', help='Set if this is an Fe-varying grid')
+parser.add_argument('--lawrence_nacs', action='store_true', help='Use Lawrence 2011 computation for NACS')
+parser.add_argument('--simple_nacs', action='store_true', help='Use simple weighted sum computation for NACS')
+args = parser.parse_args()
 
-output_dir = '/Users/hannahrae/data/dan/M1R_homogeneous_tAPXS_SB_FT_bAPXS_SB_FT_SB_FT_WEH06_01_Cl03_015/mcnp_outputs_nacs'
-mcnp_dir = '/Users/hannahrae/data/dan/M1R_homogeneous_tAPXS_SB_FT_bAPXS_SB_FT_SB_FT_WEH06_01_Cl03_015/mcnp_outputs'
-cl_grid = True # cl_grid is True with Cl-variation grid
+def nacs_lawrence2011(acs, mass_fraction, nuclide):
+    # Ignore the .80c
+    if '.80c' in nuclide:
+        nuclide = nuclide[:-4]
+    return (acs*N_A*mass_fraction)/(int(nuclide)/100.)
+
+def nacs_simple(acs, mass_fraction):
+    return acs*mass_fraction
 
 def compute_nacs(mass_fractions):
     nacs = 0
@@ -22,11 +35,15 @@ def compute_nacs(mass_fractions):
             this_acs = nacs_by_iso.loc[nacs_by_iso['nuclide'] == int(nuclide[:-4]), 'ACS'].item()
         else:
             this_acs = nacs_by_iso.loc[nacs_by_iso['nuclide'] == int(nuclide), 'ACS'].item()
-        nacs += this_acs*mass_fractions[nuclide]
+        if args.simple_nacs:
+            nacs += nacs_simple(this_acs, mass_fractions[nuclide])
+        elif args.lawrence_nacs:
+            nacs += nacs_lawrence2011(this_acs, mass_fractions[nuclide], nuclide)
+        #nacs += this_acs*mass_fractions[nuclide]
     return nacs
 
 # Go through each output file
-for f in glob(os.path.join(mcnp_dir, '*')):
+for f in glob(os.path.join(args.source_dir, '*')):
     # Read in the mass fractions for each isotope for each file
     mf = {}
     reading_m = False
@@ -44,12 +61,12 @@ for f in glob(os.path.join(mcnp_dir, '*')):
     # Compute the thermal neutron absorption cross section for this material
     nacs = compute_nacs(mf)
     # Copy the output file to a new file with NACS appended to the filename
-    if cl_grid:
+    if args.cl_grid:
         # pre-pend 14.22Fe to the start of the filenames (constant Fe)
         # insert ACS after Cl
-        name_pre, name_post = f.split('/')[-1].split('CL')
+        name_pre, name_post = f.split('/')[-1].split('Cl')
         new_name = '14.22Fe_%sCl_%sACS%s' % (name_pre, str(round(nacs, 3)), name_post)
-    else:
+    elif args.fe_grid:
         name_pre, name_post = f.split('/')[-1].split('Cl')
         new_name = '%sCl_%sACS%s' % (name_pre, str(round(nacs, 3)), name_post)
-    call(['cp', f, os.path.join(output_dir, new_name)])
+    call(['cp', f, os.path.join(args.sink_dir, new_name)])
