d124acd0f25779930c21bbe663caf6e85376fc35
hannah-rae <hkerner@asu.edu>
2019-05-11 11:34:49 -0700

    Make reduced table from IKI table

--- util/prep_dan_iki.py
+++ util/prep_dan_iki.py
@@ -1,17 +1,18 @@
 import csv
 
-datfile = '/Users/hannahrae/data/dan_iki_reduced.csv'
-outfile = '/Users/hannahrae/data/dan_iki_params.csv'
+datfile = '/Users/hannahrae/data/dan/dan_iki_reduced.csv'
+outfile = '/Users/hannahrae/data/dan/dan_iki_params_single.csv'
 
 quantities = []
 with open(datfile, 'rb') as csvfile:
     reader = csv.reader(open(datfile, 'rU'), dialect=csv.excel_tab)
     for row in reader:
         [site, sol, h2o_single, cl_single, p_single, h2o_double, cl_double, p_double] = row[0].rstrip().split(',')
-        if float(p_single) > float(p_double):
-            quantities.append([site, sol, float(h2o_single), float(cl_single)])
-        else:
-            quantities.append([site, sol, float(h2o_double), float(cl_double)])
+        quantities.append([site, sol, float(h2o_single), float(cl_single)])
+        # if float(p_single) > float(p_double):
+        #     quantities.append([site, sol, float(h2o_single), float(cl_single)])
+        # else:
+        #     quantities.append([site, sol, float(h2o_double), float(cl_double)])
 
 with open(outfile, 'wb') as csvfile:
     writer = csv.writer(csvfile)
