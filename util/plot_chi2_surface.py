0659ac9c3b349a8bae2f68a637c95a52c54653ed
hannah-rae <hkerner@asu.edu>
2019-05-15 16:52:54 -0700

    Change threshold to chi2 critical value

--- util/plot_chi2_surface.py
+++ util/plot_chi2_surface.py
@@ -11,6 +11,8 @@ import os.path
 
 from glob import glob
 
+t = 30.578 # critical value threshold for p=0.01
+
 # Go through each sol directory
 for soldir in sorted(glob('/Users/hannahrae/data/dan/dan_asufits_hiweh/*')):
     sol = int(soldir.split('/')[-1][3:])
@@ -46,8 +48,7 @@ for soldir in sorted(glob('/Users/hannahrae/data/dan/dan_asufits_hiweh/*')):
         fig.colorbar(surf, ax=ax)
         # Mark the best fit model
         ax.scatter(x[min_ind], y[min_ind], z[min_ind], marker='+', color='pink')
-        # Draw a plane at a threshold above the best-fit value
-        t = z[min_ind] + 0.10*z[min_ind]
+        # Draw a plane at the critical value threshold
         T = X*0 + t
         ax.plot_surface(X, Y, T, color='k', alpha=0.5)
         # Get the range of values for models below that threshold
@@ -61,6 +62,7 @@ for soldir in sorted(glob('/Users/hannahrae/data/dan/dan_asufits_hiweh/*')):
         ax.set_zlabel('$\chi^2$')
         ax.set_zlim(np.min(Z), np.max(Z))
         ax.set_title('Surface Plot of $\chi^2$ Values over Model Grid')
-        # plt.show()
-        plt.savefig(os.path.join(mdir, 'chi2_surface_WEH_%s_%s-%s_BNACS%s_%s-%s.png' % (str(round(x[min_ind], 1)), str(round(err_min_weh, 1)), str(round(err_max_weh, 1)), str(round(y[min_ind], 3)), str(round(err_min_bnacs, 3)), str(round(err_max_bnacs, 3)))))
+        #print mdir
+        #plt.show()
+        plt.savefig(os.path.join(mdir, 'chi2_surface_t=%s_WEH_%s_%s-%s_BNACS%s_%s-%s.png' % (str(round(t, 3)), str(round(x[min_ind], 1)), str(round(err_min_weh, 1)), str(round(err_max_weh, 1)), str(round(y[min_ind], 3)), str(round(err_min_bnacs, 3)), str(round(err_max_bnacs, 3)))))
         plt.close()
--- util/summary_data_table.py
+++ util/summary_data_table.py
@@ -5,6 +5,8 @@ import numpy as np
 
 mnames, sols, bf_wehs, bf_bnacss, unc_wehs, unc_bnacss, chi2s = [], [], [], [], [], [], []
 
+threshold = 't=30.578' # p = 0.01
+
 # Go through each sol directory
 for soldir in sorted(glob('/Users/hannahrae/data/dan/dan_asufits_hiweh/*')):
     sol = int(soldir.split('/')[-1][3:])
@@ -16,12 +18,12 @@ for soldir in sorted(glob('/Users/hannahrae/data/dan/dan_asufits_hiweh/*')):
     for mdir in glob(os.path.join(soldir, '*')):
         mnames.append(mdir.split('/')[-1])
         # Get the best fit WEH and BNACS
-        chi2_desc = glob(os.path.join(mdir, 'chi2_surface_*.png'))[0].split('/')[-1][:-4]
-        bf_wehs.append(chi2_desc.split('_')[3])
-        bf_bnacss.append(chi2_desc.split('_')[5][5:]) # includes the word BNACS oops
+        chi2_desc = glob(os.path.join(mdir, 'chi2_surface_%s_*.png' % threshold))[0].split('/')[-1][:-4]
+        bf_wehs.append(chi2_desc.split('_')[4])
+        bf_bnacss.append(chi2_desc.split('_')[6][5:]) # includes the word BNACS oops
         # Get the uncertainty range for WEH
-        unc_wehs.append(chi2_desc.split('_')[4])
-        unc_bnacss.append(chi2_desc.split('_')[6])
+        unc_wehs.append(chi2_desc.split('_')[5])
+        unc_bnacss.append(chi2_desc.split('_')[7])
         # Get the chi-squared value for best-fit model
         with open(os.path.join(mdir, 'gridInfo_statistics.csv'), 'rb') as csvfile:
             csvreader = csv.reader(csvfile)
@@ -45,7 +47,7 @@ elevs = []
 for name in mnames:
     elevs.append(elev_dict[name])
 
-np.savetxt('/Users/hannahrae/data/dan/dan_sol-weh-bnacs-unc-chi2-elev_hiweh_sol1-2100.csv', 
-            zip(mnames, sols, bf_wehs, unc_wehs, bf_bnacss, unc_bnacss, chi2s, elevs), 
+np.savetxt('/Users/hannahrae/data/dan/dan_sol-weh-bnacs-unc_%s-chi2-elev_hiweh_sol1-2100.csv' % threshold, 
+            zip(mnames, sols, elevs, bf_wehs, unc_wehs, bf_bnacss, unc_bnacss, chi2s), 
             fmt='%s', 
             delimiter=',')
